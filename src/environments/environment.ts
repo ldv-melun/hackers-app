e9df8e2bc435036e17b28ba1f350eec7603c2dcb
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2023-03-14 14:58:54 +0100

    change key dev -> prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://api.ipstack.com/',
-    keyAPI: 'KEY_API'
+    apiBaseUrl: 'http://api.ipstack.com/',
+    keyAPI: 'b0827337a4440dc9a99c2a82e3e9fa68'
 };
